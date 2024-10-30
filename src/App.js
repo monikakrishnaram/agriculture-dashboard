0d0b867638de02b1ffce7263968ccd9641c553bb
monikakrishnaram <151124785+monikakrishnaram@users.noreply.github.com>
2024-10-30 06:22:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Dashboard from './Components/Dashboard';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Feedback from './Components/Feedback';
+import SensorDisplay from './SensorDisplay'; // Import SensorDisplay
+import CropMap from './Components/CropMap';
 
-function App() {
+import './App.css';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <CropMap/>
+        <Routes>
+       
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/sensor-data" element={<SensorDisplay />} /> {/* New route for sensor data */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
